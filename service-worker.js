cca84384dc8f43262255e7ee411634d726f4af84
Jeff Posnick <jeffy@google.com>
2014-08-13 12:43:10 -0400

    Changes to support absolute as well as relative URLs.

--- service-worker.js
+++ service-worker.js
@@ -1,107 +1,121 @@
 var CACHE_PREFIX = 'cache-v';
-self.version = 1;
+// By default, use the same directory that hosts this service worker as the base URL.
+var baseUrl = new URL('./', self.location.href);
+self.version = '1';
 
-importScripts('polyfills/idbCacheUtils.js');
-importScripts('polyfills/idbCachePolyfill.js');
-importScripts('polyfills/idbCacheStoragePolyfill.js');
-
-var log = console.log.bind(console);
-var err = console.error.bind(console);
-self.onerror = err;
-
-var baseUrl = (new URL('./', self.location.href) + '');
-function fullUrl(relativeUrl) {
-  return baseUrl + relativeUrl;
+function importPolyFills() {
+  importScripts('polyfills/idbCacheUtils.js');
+  importScripts('polyfills/idbCachePolyfill.js');
+  importScripts('polyfills/idbCacheStoragePolyfill.js');
 }
 
-self.addEventListener('install', function(e) {
-  var preCache = [
-    // TODO: Find a way to pass in a list of URLs from the main element.
-  ];
-  log('oninstall; preCache is', preCache);
+function queryParamValue(param) {
+  var regex = new RegExp(param + '=([^&]+)');
+  var match = location.search.match(regex);
+  if (match) {
+    return match[1];
+  }
+  return null;
+}
 
-  e.waitUntil(caches.create(CACHE_PREFIX + self.version).then(function(cache) {
-    return Promise.all(preCache.map(function(relativeUrl) {
-      var url = fullUrl(relativeUrl);
+function initFromUrlParams() {
+  // Allow for overriding the default baseUrl via the baseUrl query parameter
+  // passed as part of this service worker's URL.
+  var baseUrlParam = queryParamValue('baseUrl');
+  if (baseUrlParam) {
+    baseUrl = new URL(baseUrlParam, self.location.href).toString();
+    console.log('baseUrl', baseUrl);
+  }
 
-      return cache.add(url).then(function() {
-        log('Successfully cached', url);
-      }, function(error) {
-        err('Error while caching', url, error);
-      });
-    }));
-  }));
-});
+  var versionUrlParam = queryParamValue('version');
+  if (versionUrlParam) {
+    self.version = versionUrlParam;
+  }
+}
 
-self.addEventListener('fetch', function(e) {
-  var request = e.request;
-  log('onfetch; request URL is', request.url);
+function absoluteUrl(url) {
+  // If url is already an absolute URL, it will just return that.
+  // Otherwise, it will convert a relative URL into an absolute one by joining it with baseUrl.
+  return new URL(url, baseUrl).toString();
+}
 
-  // Basic read-through caching.
-  e.respondWith(
-    caches.match(request, CACHE_PREFIX + self.version).then(
-      function(response) {
-        log('  cache hit!');
-        return response;
-      },
-      function() {
-        // we didn't have it in the cache, so add it to the cache and return it
-        return caches.get(CACHE_PREFIX + self.version).then(
-          function(cache) {
-            log('  cache miss; attempting to cache at runtime.');
-
-            return cache.add(request).then(
-              function(response) {
-                return response;
-              }
-            );
-          }
-        );
-      }
-    )
-  );
-});
-
-self.addEventListener('message', function(e) {
-  log('onmessage; event is', e);
-
-  caches.get(CACHE_PREFIX + self.version).then(function(cache) {
-    var url = fullUrl(e.data.relativeUrl);
-    switch(e.data.command) {
-      case 'status':
-        // TODO: There's clearly a much better way of doing this.
-        cache.keys().then(function(requests) {
-          var cached = false;
-          for(var i = 0; i < requests.length && !cached; i++) {
-            if (requests[i].url == url) {
-              cached = true;
-            }
-          }
-
-          e.data.port.postMessage({
-            relativeUrl: e.data.relativeUrl,
-            cached: cached
-          });
-        });
-      break;
-
-      case 'cache':
-        cache.add(url).then(function() {
-          e.data.port.postMessage({
-            relativeUrl: e.data.relativeUrl,
-            cached: true
-          });
-        });
-      break;
-
-      case 'uncache':
-        cache.delete(url).then(function() {
-          e.data.port.postMessage({
-            relativeUrl: e.data.relativeUrl,
-            cached: false
-          });
-        });
-      break;
-    }
+function addEventListeners() {
+  self.addEventListener('install', function(e) {
+    e.waitUntil(caches.create(CACHE_PREFIX + self.version));
   });
-});
+
+  self.addEventListener('fetch', function(e) {
+    var request = e.request;
+    console.log('onfetch; request URL is', request.url);
+
+    // Basic read-through caching.
+    e.respondWith(
+      caches.match(request, CACHE_PREFIX + self.version).then(
+        function(response) {
+          console.log('  cache hit!');
+          return response;
+        },
+        function() {
+          // we didn't have it in the cache, so add it to the cache and return it
+          return caches.get(CACHE_PREFIX + self.version).then(
+            function(cache) {
+              console.log('  cache miss; attempting to cache at runtime.');
+
+              return cache.add(request).then(function(response) {
+                return response;
+              });
+            }
+          );
+        }
+      )
+    );
+  });
+
+  self.addEventListener('message', function(e) {
+    console.log('onmessage; event is', e);
+
+    caches.get(CACHE_PREFIX + self.version).then(function(cache) {
+      var url = absoluteUrl(e.data.url);
+      switch (e.data.command) {
+        case 'status':
+          // TODO: There's clearly a much better way of doing this.
+          cache.keys().then(function(requests) {
+            var cached = false;
+            for (var i = 0; i < requests.length && !cached; i++) {
+              if (requests[i].url == url) {
+                cached = true;
+              }
+            }
+
+            e.data.port.postMessage({
+              url: e.data.url,
+              cached: cached
+            });
+          });
+        break;
+
+        case 'cache':
+          cache.add(url).then(function() {
+            e.data.port.postMessage({
+              url: e.data.url,
+              cached: true
+            });
+          });
+        break;
+
+        case 'uncache':
+          cache.delete(url).then(function() {
+            e.data.port.postMessage({
+              url: e.data.url,
+              cached: false
+            });
+          });
+        break;
+      }
+    });
+  });
+}
+
+importPolyFills();
+initFromUrlParams();
+addEventListeners();
