8d74c7c8dab6620f0844e66fd2251515bbf00b4b
Jeff Posnick <jeffy@google.com>
2014-08-14 14:48:55 -0400

    seed gh-pages

---
+++ components/service-worker-cache/README.md
@@ -0,0 +1,5 @@
+service-worker-cache
+===
+The [live demo](https://jeffposnick.github.io/service-worker-cache/components/service-worker-cache/demo.html) works in the current [Chrome Canary](https://www.google.com/chrome/browser/canary.html) with the [experimental web features](chrome://flags/#enable-experimental-web-platform-features) flag turned on.
+
+See the [component landing page](https://jeffposnick.github.io/service-worker-cache) for information about using this as a Polymer element.
---
+++ components/service-worker-cache/service-worker.js
@@ -0,0 +1,160 @@
+var CACHE_PREFIX = 'cache-v';
+// By default, use the same directory that hosts this service worker as the base URL.
+var baseUrl = new URL('./', self.location.href);
+self.version = '1';
+var precacheUrls = [];
+var urlToFallbackUrl = {};
+var urlToFallbackData = {};
+
+function importPolyFills() {
+  importScripts('polyfills/idbCacheUtils.js');
+  importScripts('polyfills/idbCachePolyfill.js');
+  importScripts('polyfills/idbCacheStoragePolyfill.js');
+}
+
+function queryParamValue(param) {
+  var regex = new RegExp(param + '=([^&]+)');
+  var match = location.search.match(regex);
+  if (match) {
+    return match[1];
+  }
+  return null;
+}
+
+function initFromUrlParams() {
+  // Allow for overriding the default baseUrl via the baseUrl query parameter
+  // passed as part of this service worker's URL.
+  var baseUrlParam = queryParamValue('baseUrl');
+  if (baseUrlParam) {
+    baseUrl = new URL(decodeURIComponent(baseUrlParam), self.location.href).toString();
+  }
+
+  var versionUrlParam = queryParamValue('version');
+  if (versionUrlParam) {
+    self.version = decodeURIComponent(versionUrlParam);
+  }
+
+  var precacheParam = queryParamValue('precache');
+  if (precacheParam) {
+    precacheUrls = precacheParam.split(',').map(function(url) {
+      return decodeURIComponent(url);
+    });
+  }
+}
+
+function absoluteUrl(url) {
+  // If url is already an absolute URL, it will just return that.
+  // Otherwise, it will convert a relative URL into an absolute one by joining it with baseUrl.
+  return new URL(url, baseUrl).toString();
+}
+
+function addEventListeners() {
+  self.addEventListener('install', function(e) {
+    // Pre-cache everything in precacheUrls, and wait until that's done to complete the install.
+    e.waitUntil(caches.create(CACHE_PREFIX + self.version).then(
+      function(cache) {
+        return Promise.all(precacheUrls.map(function(url) {
+          return cache.add(absoluteUrl(url));
+        }));
+      }
+    ));
+  });
+
+  self.addEventListener('fetch', function(e) {
+    var request = e.request;
+    console.log('onfetch; request url is', request.url);
+
+    // Basic read-through caching.
+    e.respondWith(
+      caches.match(request, CACHE_PREFIX + self.version).then(function(response) {
+        console.log('  cache hit!');
+        return response;
+      }, function() {
+        // we didn't have it in the cache, so add it to the cache and return it
+        return caches.get(CACHE_PREFIX + self.version).then(function(cache) {
+          console.log('  cache miss; attempting to fetch and cache at runtime...');
+
+          return cache.add(request.url).then(
+            function(response) {
+              console.log('  fetch successful.');
+              return response;
+            },
+            function() {
+              if (request.url in urlToFallbackUrl) {
+                console.log('  fetch failed; falling back to', urlToFallbackUrl[request.url]);
+                return fetch(urlToFallbackUrl[request.url]);
+                // TODO: Fall back to the urlToFallbackData[request.url] if present.
+              } else {
+                console.log('  fetch failed; no fallback available.');
+              }
+            }
+          );
+        });
+      })
+    );
+  });
+
+  self.addEventListener('message', function(e) {
+    console.log('onmessage; data is', e.data);
+
+    caches.get(CACHE_PREFIX + self.version).then(function(cache) {
+      var url = absoluteUrl(e.data.url);
+      switch (e.data.command) {
+        case 'status':
+          // TODO: There's clearly a much better way of doing this.
+          cache.keys().then(function(requests) {
+            var cached = false;
+            for (var i = 0; i < requests.length && !cached; i++) {
+              if (requests[i].url == url) {
+                cached = true;
+              }
+            }
+
+            e.data.port.postMessage({
+              url: e.data.url,
+              cached: cached
+            });
+          });
+        break;
+
+        case 'cache':
+          cache.add(url).then(function() {
+            e.data.port.postMessage({
+              url: e.data.url,
+              cached: true
+            });
+          });
+        break;
+
+        case 'uncache':
+          cache.delete(url).then(function() {
+            e.data.port.postMessage({
+              url: e.data.url,
+              cached: false
+            });
+          });
+        break;
+
+        case 'registerFallbackUrl':
+          var fallbackUrl = absoluteUrl(e.data.fallbackUrl);
+          cache.add(fallbackUrl).then(function() {
+            urlToFallbackUrl[url] = fallbackUrl;
+            console.log('  cached', fallbackUrl, 'as fallback for', url);
+            e.data.port.postMessage({
+              url: e.data.fallbackUrl,
+              cached: true
+            });
+          });
+        break;
+
+        case 'registerFallbackData':
+          urlToFallbackData[url] = e.data.fallbackData;
+        break;
+      }
+    });
+  });
+}
+
+importPolyFills();
+initFromUrlParams();
+addEventListeners();
