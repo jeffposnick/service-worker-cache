693dff7cfbd852c7934cfbe0e3e534a76a63c6ba
Mat Scales <mscales@google.com>
2014-11-11 21:57:17 +0000

    Fully implemented the fallback feature, using a second Cache

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,11 @@
-var CACHE_PREFIX = 'cache-v';
 var DEFAULT_BASE_URL = './';
 var DEFAULT_VERSION = 1;
 
 var precacheUrls;
 var baseUrl;
 var version;
-
-// TODO: This shouldn't be here. Don't store global state which will be lost across SW restarts.
-var urlToFallbackUrl = {};
-var urlToFallbackData = {};
+var networkCacheName = 'network:' + self.scope + ':';
+var fallbackCacheName = 'fallback:' + self.scope + ':';
 
 function importPolyFills() {
   importScripts('../cache-polyfill/dist/serviceworker-cache-polyfill.js');
@@ -28,87 +25,90 @@ function initFromUrlParams() {
   var params = deserializeUrlParams(location.search.substring(1));
 
   // Allow some defaults to be overridden via URL parameters.
-  // TODO: Confirm that this is called even when this isn't the initial install. Otherwise, the
-  // values should be saved to IndexedDB.
   baseUrl = new URL(params.has('baseUrl') ? params.get(baseUrl) : DEFAULT_BASE_URL, self.location.href).toString();
   version = params.has('version') ? params.get('version') : DEFAULT_VERSION;
+  networkCacheName += version;
+  fallbackCacheName += version;
   precacheUrls = params.has('precache') ? params.get('precache').split(',') : [];
 }
 
-function absoluteUrl(url) {
-  // If url is already an absolute URL, it will just return that.
-  // Otherwise, it will convert a relative URL into an absolute one by joining it with baseUrl.
-  return new URL(url, baseUrl).toString();
+function getNetworkCache() {
+  return caches.open(networkCacheName);
+}
+
+function getFallbackCache() {
+  return caches.open(fallbackCacheName);
 }
 
 function addEventListeners() {
-  self.addEventListener('install', function(e) {
+  self.addEventListener('install', function(event) {
     // Pre-cache everything in precacheUrls, and wait until that's done to complete the install.
-    e.waitUntil(caches.open(CACHE_PREFIX + self.version).then(function(cache) {
-      return cache.addAll(precacheUrls.map(absoluteUrl));
-    }));
-  });
-
-  self.addEventListener('fetch', function(e) {
-    var request = e.request;
-
-    // Basic read-through caching.
-    e.respondWith(
-      caches.match(request).then(function(response) {
-        if (response) {
-          console.log('  cache hit!');
-          return response;
-        } else {
-          // we didn't have it in the cache, so add it to the cache and return it
-          return caches.open(CACHE_PREFIX + self.version).then(function(cache) {
-            console.log('  cache miss; attempting to fetch and cache at runtime...');
-
-            fetch(request.clone()).then(function(response) {
-              console.log('  fetch successful.');
-              cache.put(request, response.clone());
-              return response;
-            }, function() {
-              if (request.url in urlToFallbackUrl) {
-                console.log('  fetch failed; falling back to', urlToFallbackUrl[request.url]);
-                return fetch(urlToFallbackUrl[request.url]);
-                // TODO: Fall back to the urlToFallbackData[request.url] if present.
-              } else {
-                console.log('  fetch failed; no fallback available.');
-              }
-            });
-          });
-        }
+    event.waitUntil(
+      Promise.all([
+        getNetworkCache(),
+        getFallbackCache()
+      ]).then(function(caches) {
+        return caches[0].addAll(precacheUrls);
       })
     );
   });
 
-  self.addEventListener('message', function(e) {
-    console.log('onmessage; data is', e.data);
+  self.addEventListener('activate', function(event) {
+    // TODO: Tidy up old caches
+  });
 
-    caches.open(CACHE_PREFIX + self.version).then(function(cache) {
-      var url = absoluteUrl(e.data.url);
-      switch (e.data.command) {
-        case 'status':
-          // TODO: There's clearly a much better way of doing this.
-          cache.keys().then(function(requests) {
-            var cached = false;
-            for (var i = 0; i < requests.length && !cached; i++) {
-              if (requests[i].url == url) {
-                cached = true;
+  self.addEventListener('fetch', function(event) {
+    var request = event.request;
+
+    // Basic read-through caching.
+    event.respondWith(
+      getNetworkCache().then(function(networkCache) {
+        return networkCache.match(request).then(function(response) {
+          if (response) {
+            console.log('  cache hit!');
+            return response;
+          } else {
+            // we didn't have it in the cache, so add it to the cache and return it
+            console.log('  cache miss; attempting to fetch and cache at runtime...');
+
+            return fetch(request.clone()).then(function(response) {
+              if (response.status >= 400) {
+                return Promise.reject(new Error(response.statusText));
               }
-            }
+              console.log('  fetch successful.');
+              networkCache.put(request, response.clone());
+              return response;
+            }).catch(function() {
+              console.log('  fetch failed, trying the fallback cache.');
+              return getFallbackCache().then(function(fallbackCache) {
+                return fallbackCache.match(request);
+              });
+            });
+          }
+        });
+      })
+    );
+  });
 
-            e.data.port.postMessage({
-              url: e.data.url,
-              cached: cached
+  self.addEventListener('message', function(event) {
+    console.log('onmessage; data is', event.data);
+
+    getNetworkCache().then(function(cache) {
+      var url = event.data.url;
+      switch (event.data.command) {
+        case 'status':
+          cache.match(event.data.url).then(function(response) {
+            event.data.port.postMessage({
+              url: url,
+              cached: !!response
             });
           });
         break;
 
         case 'cache':
           cache.add(url).then(function() {
-            e.data.port.postMessage({
-              url: e.data.url,
+            event.data.port.postMessage({
+              url: url,
               cached: true
             });
           });
@@ -116,27 +116,30 @@ function addEventListeners() {
 
         case 'uncache':
           cache.delete(url).then(function() {
-            e.data.port.postMessage({
-              url: e.data.url,
+            event.data.port.postMessage({
+              url: url,
               cached: false
             });
           });
         break;
 
         case 'registerFallbackUrl':
-          var fallbackUrl = absoluteUrl(e.data.fallbackUrl);
-          cache.add(fallbackUrl).then(function() {
-            urlToFallbackUrl[url] = fallbackUrl;
-            console.log('  cached', fallbackUrl, 'as fallback for', url);
-            e.data.port.postMessage({
-              url: e.data.fallbackUrl,
-              cached: true
+          getFallbackCache().then(function(fallbackCache) {
+            fetch(event.data.fallbackUrl).then(function(response) {
+              fallbackCache.put(url, response);
+              console.log('  cached', event.data.fallbackUrl, 'as fallback for', url);
+              event.data.port.postMessage({
+                url: event.data.fallbackUrl,
+                cached: true
+              });
             });
           });
         break;
 
         case 'registerFallbackData':
-          urlToFallbackData[url] = e.data.fallbackData;
+          getFallbackCache().then(function(fallbackCache) {
+            fallbackCache.put(url, new Response(event.data.fallbackData));
+          });
         break;
       }
     });
